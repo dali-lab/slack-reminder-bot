5be7060fb4a9b79eb43ac704d4471acfff0e591c
harquail <nook@harquail.com>
2015-07-24 00:47:47 -0400

    seems like it should work

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require("express");
 var logfmt = require("logfmt");
 var Slack = require("slack-client");
 var request = require('request');
+var GoogleSpreadsheet = require('google-spreadsheet');
 var app = express();
 var port = process.env.PORT || 5000;
 var bodyParser = require('body-parser');
@@ -21,52 +22,38 @@ var token = process.env.SLACK_BOT_TOKEN, // Add a bot at https://my.slack.com/se
 autoReconnect = true,
 autoMark = true;
 
+var reminders_sheet = new GoogleSpreadsheet(process.env.REMINDERS_GOOGLE_URL);
+var groups_sheet = new GoogleSpreadsheet(process.env.GROUP_DATES_GOOGLE_URL);
+var google_creds = {
+  client_email: process.env.CLIENT_EMAIL,
+  private_key: process.env.PRIVATE_KEY
+}
+
+
 var slack = new Slack(token, autoReconnect, autoMark);
 slack.login();
 
 var lastUrgentMessageTime = 0.0;
 
+
+
 slack.on('open', function() {
 
-  var channels = [],
-  groups = [],
-  unreads = slack.getUnreadCount(),
-  key;
-
-  /// SCHEDULING CODE
-  // set later to use local time
-  later.date.localTime();
-  // this is 8 PM on July 23rd
-  var s = later.parse.recur().every(1).second().on(20).hour().on(7).month().on(23).dayOfMonth().on(2015).year();
-  var curChan = slack.getChannelGroupOrDMByName("#info");
-  var timer2 = later.setTimeout(sendMessageToChannel.bind(null,curChan,"hi"), s);
-
-  var curChan = slack.getChannelGroupOrDMByName("nook_portfolio");
-  var timer3 = later.setTimeout(sendMessageToChannel.bind(null,curChan,"hello there"), s);
-
- function sendMessageToChannel(curr,message) {
-   curr.send(message);
-   console.log("TRIGGERED");
- }
- /// SCHEDULING CODE
-
-  for (key in slack.channels) {
-
-    // console.log(slack.channels[key].name);
-    // slack.joinChannel(slack.channels[key].name,console.log);
-
-    if (slack.channels[key].is_member) {
-      channels.push('#' + slack.channels[key].name);
-    }
+  dailyCheck();
+  
   }
 
-  for (key in slack.groups) {
-    if (slack.groups[key].is_open && !slack.groups[key].is_archived) {
-      groups.push(slack.groups[key].name);
-    }
+  // once a day, check for reminders to send
+  function dailyCheck(){
+    // set later to use local time
+    later.date.localTime();
+    // this is 8 PM on July 23rd
+    var elevenAM = later.parse.recur().every(1).dayOfMonth().on(11).hour();
+    var tenAM = later.parse.recur().every(1).dayOfMonth().on(10).hour();
+
+    var timer2 = later.setInterval(remindPresentingGroups, elevenAM);
+    var timer3 = later.setInterval(otherReminders, tenAM);
   }
-
-
 
   console.log('Welcome to Slack. You are @%s of %s', slack.self.name, slack.team.name);
   console.log('You are in: %s', channels.join(', '));
@@ -74,12 +61,78 @@ slack.on('open', function() {
   console.log('You have %s unread ' + (unreads === 1 ? 'message' : 'messages'), unreads);
 });
 
+function consoleLog(){
+  console.log("one");
+}
+
+function otherReminders(){
+      // getInfo returns info about the sheet and an array or "worksheet" objects
+      reminders_sheet.getInfo(function(err, sheet_info) {
+        var config_sheet = sheet_info.worksheets.filter( function(worksheet) {
+          return worksheet.title == 'CONFIGS';
+        })[0] || sheet_info.worksheets[0];
+
+        var now = new Date();
+
+        // gets the rows and config values
+        config_sheet.getRows(function(err, rows) {
+          for (var i in rows){
+            var dates = rows[i].date.split('/');
+            var dateToRemind = new Date(dates[2],dates[0]-1,dates[1]);
+
+            if(datesAreEquivalent(now,dateToRemind)){
+                var channel = slack.getChannelGroupOrDMByName("#general");
+                channel.send("<!everyone> Hey DALI — this is a reminder about "+rows[i].reminder+" today! :cat:");
+            }
+
+          }
+        });
+      });
+}
+
+function remindPresentingGroups(){
+  // getInfo returns info about the sheet and an array or "worksheet" objects
+  groups_sheet.getInfo(function(err, sheet_info) {
+    var config_sheet = sheet_info.worksheets.filter( function(worksheet) {
+      return worksheet.title == 'CONFIGS';
+    })[0] || sheet_info.worksheets[0];
+
+    var now = new Date();
+    // gets the rows and config values
+    config_sheet.getRows(function(err, rows) {
+      for (var i in rows){
+        var dates = rows[i].date.split('/');
+        var dateToRemind = new Date(dates[2],dates[0]-1,dates[1]);
+        // get date — remind groups 3 days before their presentation day
+        dateToRemind.setDate(dateToRemind.getDate()-3);
+
+        console.log("reminderDate",dateToRemind);
+        console.log("now",now);
+        if(datesAreEquivalent(now,dateToRemind)){
+
+          var groups = rows[i].groups.split(' ');
+
+          for (i in groups){
+            console.log(groups[i]);
+            var channel = slack.getChannelGroupOrDMByName(groups[i]);
+            channel.send("<!channel> Hey #"+channel.name+"— this is a reminder that your group is scheduled to present at the lab meeting on "+rows[i].date+" :tada:");
+          }
+        }
+      }
+    });
+  });
+}
+
+// day, year, and month are equivalent
+function datesAreEquivalent(date1,date2){
+  return (date1.getMonth() == date2.getMonth()) && (date1.getFullYear() == date2.getFullYear()) && (date1.getDate() == date2.getDate()) ;
+}
+
 
 slack.on('error', function(error) {
 
 console.error('Error: %s', error);
 });
-
 
 app.get('/', function(req, res) {
 res.send('Hello World!');
